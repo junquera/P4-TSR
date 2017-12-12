fbceda82e753261cdbc81604559c5715a9e6c9b6
J.Junquera <javier.junquera.sanchez@gmail.com>
2017-12-12 18:47:20 +0100

    Paho!

--- src/main.py
+++ src/main.py
@@ -1,51 +1,52 @@
-from xiPy.xively_connection_parameters import XivelyConnectionParameters
-from xiPy.xively_client import XivelyClient
-from xiPy.xively_error_codes import XivelyErrorCodes
-import credentials
-max_attempts = 3
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import ssl
 
-def attempt_gen():
-    attempt = 0
-    while attempt < max_attempts:
-        attempt += 1
-        yield attempt
+auth = {
+    'username': 'cee0d2bd-136a-4378-b7e3-760086e8d265',
+    'password': '/2sI17f4lxo6dc3nGIxpHrZO3rbYjj8DsafJNuYr5ZI='
+}
 
-def on_connect_finished(client,result):
-    if result == XivelyErrorCodes.XI_STATE_OK :
-        print("[SUCCESS] Connected to Xively")
+tls = {
+    'ca_certs': "/etc/ssl/certs/ca-certificates.crt",
+    'tls_version': ssl.PROTOCOL_TLSv1
+}
 
-    else :
-        try:
-            attempt_number = next(attempt_gen())
-        except StopIteration:
-            print("   [FAIL] Unable to connect after %d attempts." % max_attempts)
-            sys.exit(-1)
-        print("   [INFO] Connection try %d/%d"  % (attempt_number, max_attempts))
-        print("  [ERROR] Connection error :" , result)
-        print("   [INFO] Reconnecting to the broker ... ")
-        client.connect(params)
+# publish.single("/xi/blue/v1/ad4935ed-01d5-4454-b322-7e425d644612/d/cee0d2bd-136a-4378-b7e3-760086e8d265/_log",
+# payload="paho!",
+# hostname="broker.xively.com",
+# client_id="cee0d2bd-136a-4378-b7e3-760086e8d265",
+# auth=auth,
+# tls=tls,
+# port=8883,
+# protocol=mqtt.MQTTv311
+# )
+
+def on_message(client, userdata, msg):
+    print(client)
+    print(msg.topic+" "+str(msg.payload))
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe('xi/blue/v1/ad4935ed-01d5-4454-b322-7e425d644612/d/cee0d2bd-136a-4378-b7e3-760086e8d265/_log')
 
 
-# XivelyConfig.XI_MQTT_HOSTS = [("junquera.app.xively.com", 8883, True)]
-# Client connection
+client = mqtt.Client('cee0d2bd-136a-4378-b7e3-760086e8d265', clean_session=False)
+client.on_message = on_message
+client.on_connect = on_connect
+client.username_pw_set(auth['username'], password=auth['password'])
+client.tls_set(tls['ca_certs'], tls_version=ssl.PROTOCOL_TLSv1_2)
 
-client = XivelyClient()
-client.on_connect_finished = on_connect_finished
+client.connect('broker.xively.com', port=8883)
 
-params = XivelyConnectionParameters()
-params.use_websocket = False
-params.publish_count_send_time_period = 5
 
-params.device_id = credentials.xively['device_id']
-params.username = credentials.xively['device_id']
-params.password = credentials.xively['password']
 
-print("   [INFO] ClientID: %s" %(params.client_id))
-print("   [INFO] Username: %s" %(params.username))
-print("   [INFO] Password: %s" %(params.password))
-print("   [INFO] Connecting to Xively...")
+client.loop_forever()
 
-client.connect(params)
-client.join()
-
-# client.subscribe('/_updates/fields')
+client.publish('xi/blue/v1/ad4935ed-01d5-4454-b322-7e425d644612/d/cee0d2bd-136a-4378-b7e3-760086e8d265/_log', payload='paho!')
+#
+# client.disconnect()
+#
+# client.loop_stop()
